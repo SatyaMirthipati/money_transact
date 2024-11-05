ec314f6cf3706a7f5fc3a4b0197b740d8265b6b0
Sri Satya <satya@janaspandana.in>
2024-11-05 15:13:29 +0530

    Add colors.dart

---
+++ lib/resource/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const primaryColor = Color(0xFF245D6B);
+  static const divider = Colors.black12;
+  static const border = Color(0xFF11111129);
+  static const containerBorder = Color(0xFF24272C);
+  static const background = Color(0xFFF8F8F8);
+  static const card = Colors.white;
+  static const dark = Color(0xFF00063A);
+}
