937a1d5ac50c535a99f0fd6501a14e074f044b42
Sri Satya <satya@janaspandana.in>
2024-11-06 12:16:05 +0530

    Add global-key

--- lib/app.dart
+++ lib/app.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 import 'resource/theme.dart';
 import 'ui/splash/splash_screen.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: 'Money Transact',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
       home: const SplashScreen(),
