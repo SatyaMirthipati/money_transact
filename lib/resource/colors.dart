e3c69937dd7bf76a2222a539ba3a45cf7a14858d
Sri Satya <satya@janaspandana.in>
2024-11-07 15:51:07 +0530

    Change colors in colors.dart

--- lib/resource/colors.dart
+++ lib/resource/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const primaryColor = Color(0xFF245D6B);
+  static const primaryColor = Color(0xFF369A6F);
   static const divider = Colors.black12;
-  static const border = Color(0xFF11111129);
+  static const border = Colors.black26;
   static const containerBorder = Color(0xFF24272C);
   static const background = Color(0xFFF8F8F8);
   static const card = Colors.white;
